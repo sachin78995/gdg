5f4db4c317fdf543693b74fb173e92bbc05c0c03
Harish3005-cloud <harish.e.naidu@gmail.com>
2026-02-28 20:11:26 +0530

    MSG

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -52,10 +52,12 @@ const Contact = () => {
       
       // Simulate API call delay
       await emailjs.sendForm(
-        import.meta.env.VITE_APP_EMAILJS_ID,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         formRef.current,
-        import.meta.env.VITE_APP_PUBLIC_KEY,
+        {
+          publicKey: import.meta.env.VITE_APP_PUBLIC_KEY,
+        }
       )
       
       setSubmitStatus('success');
